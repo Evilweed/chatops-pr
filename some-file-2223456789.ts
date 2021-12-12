9faf0b6c7f23c8647af6c4519fc6d7a6b698e16b
Evilweed <mcierpicki@gmail.com>
2021-12-12 23:42:27 +0100

    change2aa

--- some-file-2223456789.ts
+++ some-file-2223456789.ts
@@ -1,12 +1,20 @@
-function asd(sdf) {
+function asaadsdsd(sdf) {
     console.log(sdf)
 }
 
 
-function fdlkgj(sdf) {
+function fdasadslkgj(sdf) {
     console.log(sdf)
 }
 
-function asdasddsadad(sdf) {
+function asdasdasasddsadad(sdf) {
     console.log(sdf)
+}
+
+function fdasadslkssdfgj(sdfsdfds, fdgfdg) {
+    console.log(sdfsdfds)
+}
+
+function asdasdasasddsasddfsdad(sdf,sdfdsfdfs,dgfgdgf) {
+    console.log(sdfsdfds)
 }
