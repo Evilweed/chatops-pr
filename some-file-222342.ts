82728e7c0ae4fa0b39b90b040032bbc2dc64e118
Evilweed <mcierpicki@gmail.com>
2021-12-14 23:15:20 +0100

    asdsag

--- some-file-222342.ts
+++ some-file-222342.ts
@@ -1,12 +1,4 @@
 function asaaasddsdsd(sdf) {
     console.log(sdf)
 }
-function asaaasdds222dsd(sdf) {
-    console.log(sdf)
-}
-
-
-function asaaasddssdfsdf222dsd(sdf) {
-    console.log(sdf)
-}
 
