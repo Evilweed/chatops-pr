42b4053612164bedda43d75f15bb64323d938c9c
Evilweed <mcierpicki@gmail.com>
2021-12-15 14:44:37 +0100

    reformatting comment

--- some-file-222342.ts
+++ some-file-222342.ts
@@ -2,3 +2,12 @@ function asaaasddsdsd(sdf) {
     console.log(sdf)
 }
 
+function asaaaasdsddsdsd(sdf) {
+    console.log(sdf)
+}
+
+
+function asaaaasdsadasdsddsdsd(sdf) {
+    console.log(sdf)
+}
+
--- some-file-2223456789.ts
+++ some-file-2223456789.ts
@@ -9,15 +9,3 @@ function fdasadslkgj(sdf) {
 function asdasdasasddsadad(sdf) {
     console.log(sdf)
 }
-
-function fdasadslkssdfgj(sdfsdfds, fdgfdg) {
-    console.log(sdfsdfds)
-}
-
-function asdasdaasdadssasddsadad(sdf) {
-    console.log(sdf)
-}
-
-function fdasadassadslkssdfgj(sdfsdfds, fdgfdg) {
-    console.log(sdfsdfds)
-}
