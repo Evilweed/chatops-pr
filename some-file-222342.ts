5516d7f58d1bede34303967d6d80b1ca17942b27
Evilweed <mcierpicki@gmail.com>
2021-12-15 20:31:26 +0100

    formatting

--- some-file-222342.ts
+++ some-file-222342.ts
@@ -1,1 +1,32 @@
 // asd
+function asaadsdsd(sdf) {
+    console.log(sdf)
+}
+
+function asaadsdasdsadasdasdssd(sdf) {
+    console.log(sdf)
+}
+
+function asaadsfdsfsdfdssdsd(sdf) {
+    console.log(sdf)
+}
+
+function asaadsddfgdfasdasdssd(sdf) {
+    console.log(sdf)
+}
+
+function asaadsdfghfghsd(sdf) {
+    console.log(sdf)
+}
+
+function asaadsdaghjghjjghsdasdssd(sdf) {
+    console.log(sdf)
+}
+
+function asaadhkjkhjsdsd(sdf) {
+    console.log(sdf)
+}
+
+function asaadsdajlkjkjklsdasdssd(sdf) {
+    console.log(sdf)
+}
