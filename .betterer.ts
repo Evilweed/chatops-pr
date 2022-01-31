f1d6a55169aca1debaf330f7693d57766d5e2ea8
Evilweed <mcierpicki@gmail.com>
2022-01-31 20:12:49 +0100

    changes3

--- .betterer.ts
+++ .betterer.ts
@@ -1,37 +1,21 @@
 const path = require('path');
 import { typescript } from '@betterer/typescript'
 
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
-console.log(path.resolve(__dirname, 'src/**/*.ts'))
+const pathToTypescriptFiles = 'src/**/*.ts'
+const pathToTypescriptConfigFile = 'tsconfig.json'
 
+const resolvedPathToTypescriptFiles = path.resolve(__dirname, pathToTypescriptFiles)
+const resolvedPathToTypescriptConfigFile = path.resolve(__dirname, pathToTypescriptConfigFile)
+console.log(resolvedPathToTypescriptFiles)
 
 export default {
     'stricter typescript compilation': () =>
-        typescript('tsconfig.json', {
+        typescript(resolvedPathToTypescriptConfigFile, {
             strict: true,
             noImplicitAny: true,
             strictNullChecks: true,
             strictFunctionTypes: true,
             strictPropertyInitialization: false,
             useDefineForClassFields: false,
-        }).include(path.resolve(__dirname, 'src/**/*.ts')),
+        }).include(resolvedPathToTypescriptFiles),
 }
