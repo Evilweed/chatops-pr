03335ebf00d670cf73b5b1251c9c4e64652e87cb
Evilweed <mcierpicki@gmail.com>
2022-01-31 20:06:24 +0100

    print

--- .betterer.ts
+++ .betterer.ts
@@ -1,6 +1,29 @@
 const path = require('path');
 import { typescript } from '@betterer/typescript'
 
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+console.log(path.resolve(__dirname, 'src/**/*.ts'))
+
+
 export default {
     'stricter typescript compilation': () =>
         typescript('tsconfig.json', {
