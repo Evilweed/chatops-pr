cb0ddc59634e8215032c7456824949cb3b436e54
Evilweed <mcierpicki@gmail.com>
2022-02-01 12:59:34 +0100

    testz

--- src/some-file-222342.ts
+++ src/some-file-222342.ts
@@ -12,6 +12,10 @@ function asaadhkjkhjsdsd(sdf) {
     console.log(sdf)
 }
 
+function asaadhkjkhjaaaaaasdsd(sdf) {
+    console.log(sdf)
+}
+
 function asaadsdajlkjkjklsdasdssd(sdf) {
     console.log(sdf)
 }
