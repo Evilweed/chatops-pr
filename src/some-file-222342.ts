5670ad1e9615ce4d6e092614ebf3cf4165d6a97b
Evilweed <mcierpicki@gmail.com>
2022-02-02 21:09:05 +0100

    fixed some typescript offences

--- src/some-file-222342.ts
+++ src/some-file-222342.ts
@@ -16,7 +16,7 @@ function asaadsdasjsdaaaafdssfaasadfafsdafssasadasddlkjkjklsdasdssd(sdf) {
     console.log(sdf)
 }
 
-function asaadsdajaasdaaaaasadjaaahgjhaskdsdfdsfsdffsadfssaasadasddlkjkjklsdasdssd(sdf) {
+function asaadsdajaasdaaaaasadjaaahgjhaskdsdfdlsfsdffsadfssaasadasddlkjkjklsdasdssd(sdf) {
     console.log(sdf)
 }
 
