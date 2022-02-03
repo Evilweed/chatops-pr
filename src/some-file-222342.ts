fb48734f663352a48b454a494192930075626cbd
Evilweed <mcierpicki@gmail.com>
2022-02-03 18:17:24 +0100

    fixed some typescript offences

--- src/some-file-222342.ts
+++ src/some-file-222342.ts
@@ -16,7 +16,7 @@ function asaadsdasjsdaaaaaafdsaasfaasadfafsdafssasadasddlkjkjklsdasdssd(sdf) {
     console.log(sdf)
 }
 
-function asaadsdajaasdaaaaaaaasadaasaaddsaaasajaaaaasdfghagjasahaaaaasakdsdfdlaadgsafsdffsadfssaasadasddlkjakjklsdasdssd(sdf) {
+function asaadsdajaasdaaaaaaaaasadaasaaddsaaasajaaaaasdfghagjasahaaaaasakdsdfdlaadgsafsdffsadfssaasadasddlkjakjklsdasdssd(sdf) {
     console.log(sdf)
 }
 function asaadsdajaasdaaaaasadajaaahgasdjhasakdsdfddlaadgsafsdffsadfssaasadasddlkjakjklsdasdssd(sdf) {
